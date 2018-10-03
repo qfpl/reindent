9db37d9e01a4e8c4254a5b9feb27f1704db33e86
George Wilson <george@wils.online>
2018-10-03 11:14:25 +1000

    Simple command line options parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,26 @@
 module Main where
 
+import Control.Applicative ((<$>), (<**>), many)
+import qualified Options.Applicative as O
+import Language.Python.Parse
+
+data IndentOptions =
+  IndentOptions [FilePath]
+  deriving (Eq, Ord, Show)
+
+indentOptions :: O.Parser IndentOptions
+indentOptions = IndentOptions <$> many pyfile <**> O.helper
+
+pyfile :: O.Parser FilePath
+pyfile =
+  O.strArgument $
+    mconcat [O.metavar "python file", O.help "Python source file to reindent"]
+
+parseOpts :: IO IndentOptions
+parseOpts = O.execParser . O.info indentOptions $ mconcat
+  [O.fullDesc, O.header "reindent - fix inconsistent python indentation"]
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  opts <- parseOpts
+  print opts
