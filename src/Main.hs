73e226c351d3a8c4a62a185bc253a1686e05cec9
George Wilson <george@wils.online>
2018-10-03 13:38:19 +1000

    Manipulate indentation (just remove it all for now)

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,52 @@
 module Main where
 
-import Control.Applicative ((<$>), (<**>), many)
+import Control.Applicative ((<$>), (<**>), liftA2, many)
+import Control.Lens
+import Data.Foldable (traverse_, for_)
+import Data.List.NonEmpty (NonEmpty ((:|)))
 import qualified Options.Applicative as O
-import Language.Python.Parse
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+import Data.Validation (Validation (Success, Failure))
+
+import Language.Python.Parse as HPY
+import Language.Python.Internal.Optics
+import Language.Python.Internal.Render
+import Language.Python.Internal.Syntax
+import Language.Python.Internal.Syntax.Whitespace
 
 data IndentOptions =
-  IndentOptions [FilePath]
+  IndentOptions
+    { optFiles :: NonEmpty FilePath
+    }
   deriving (Eq, Ord, Show)
 
 indentOptions :: O.Parser IndentOptions
-indentOptions = IndentOptions <$> many pyfile <**> O.helper
+indentOptions = IndentOptions <$> many1 pyfile <**> O.helper
+  where
+    many1 p = liftA2 (:|) p (many p)
 
 pyfile :: O.Parser FilePath
 pyfile =
   O.strArgument $
-    mconcat [O.metavar "python file", O.help "Python source file to reindent"]
+    mconcat [O.metavar "python-file", O.help "Python source file to reindent"]
 
 parseOpts :: IO IndentOptions
 parseOpts = O.execParser . O.info indentOptions $ mconcat
   [O.fullDesc, O.header "reindent - fix inconsistent python indentation"]
 
+desiredInd :: Indent
+desiredInd = review indentWhitespaces []
+
 main :: IO ()
 main = do
   opts <- parseOpts
-  print opts
+  files <- traverse (\x -> traverse Text.readFile (x,x)) (optFiles opts)
+  let parsedModules = traverse (uncurry HPY.parseModule) files
+  case parsedModules of
+    Failure e -> do
+      putStrLn "The following errors occurred:"
+      traverse_ print e
+    Success mods -> do
+      let mods' = over (_Statements._Indents.indentsValue.traverse) (const desiredInd) <$> mods
+      traverse_ (Text.putStrLn . showModule) mods'
---
+++ test-files/test.py
@@ -0,0 +1,21 @@
+def fact(x):
+	ret = 1
+	if x >= 1:
+		for i in range(1,x+1):
+			ret = ret * i
+	return ret
+
+def fib(x):
+  if x < 2:
+    return 1
+  else:
+    return fib(x-1) + fib(x-2)
+
+def main():
+    print(fib(5))
+    print(fact(7))
+
+if 1 == 2:
+   main()
+else:
+     main()
