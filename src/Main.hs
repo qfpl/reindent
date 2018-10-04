944668ef822c076cd7f4add102c5f320ae999c85
George Wilson <george@wils.online>
2018-10-04 11:18:59 +1000

    Overwrite a file rather than printing to stdout

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,23 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveTraversable #-}
+
 module Main where
 
 import Control.Applicative ((<$>), (<**>), (<|>), liftA2, many)
 import Control.Lens
-import Data.Foldable (traverse_, for_)
-import Data.List.NonEmpty (NonEmpty ((:|)))
-import qualified Options.Applicative as O
-import qualified Data.Text as Text
+import Data.Foldable (traverse_)
+import Data.List.NonEmpty (NonEmpty)
+import Data.Text (Text)
 import qualified Data.Text.IO as Text
 import Data.Validation (Validation (Success, Failure))
+import qualified Options.Applicative as O
 
 import Language.Python.Parse as HPY
 import Language.Python.Internal.Optics
 import Language.Python.Internal.Render
 import Language.Python.Internal.Syntax
-import Language.Python.Internal.Syntax.Whitespace
 
 
 ---- Options parsing
@@ -37,7 +41,7 @@ indentationOpts =
       tab = O.switch $ mconcat
         [ O.long "tabs"
         , O.help "Replace indentation with tabs"]
-      spaces = O.option (O.auto) (mconcat
+      spaces = O.option O.auto (mconcat
         [ O.long "spaces"
         , O.metavar "num"
         , O.help "Replace indentation with spaces"
@@ -54,22 +58,40 @@ parseOpts = O.execParser . O.info appOptions $ mconcat
 
 
 ---- Indentation stuff
+setStatementIndents :: [Whitespace] -> Statement '[] a -> Statement '[] a
 setStatementIndents desired = transform (_Indent .~ desired)
 
+setModuleIndents :: [Whitespace] -> Module '[] a -> Module '[] a
 setModuleIndents = over _Statements . setStatementIndents
 
+
+---- File IO
+
+data Named a =
+  Named {
+    name  :: FilePath
+  , value :: a
+  } deriving (Functor, Foldable, Traversable)
+
+writeFileN :: Named Text -> IO ()
+writeFileN (Named fp text) = Text.writeFile fp text
+
+parseModuleN :: Named Text -> Validation (NonEmpty (ParseError SrcInfo)) (Named (Module '[] SrcInfo))
+parseModuleN (Named n a) = Named n <$> HPY.parseModule n a
 
 ---- main
 main :: IO ()
 main = do
   opts <- parseOpts
   let desiredIndent = desiredIndentation opts
-  files <- traverse (\x -> traverse Text.readFile (x,x)) (optFiles opts)
-  let parsedModules = traverse (uncurry HPY.parseModule) files
+  files <- traverse (\fp -> traverse Text.readFile (Named fp fp)) (optFiles opts)
+  let
+    parsedModules :: Validation (NonEmpty (ParseError SrcInfo)) [Named (Module '[] SrcInfo)]
+    parsedModules = traverse parseModuleN files
   case parsedModules of
     Failure e -> do
       putStrLn "The following errors occurred:"
       traverse_ print e
     Success mods -> do
-      let mods' = fmap (setModuleIndents desiredIndent) mods
-      traverse_ (Text.putStrLn . showModule) mods'
+      let mods' = fmap (fmap (setModuleIndents desiredIndent)) mods
+      traverse_ (writeFileN . fmap showModule) mods'
