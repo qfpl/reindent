22dd6ee03a52c1bebcf722ccca18397e438717b6
Isaac Elliott <isaace71295@gmail.com>
2018-10-22 13:43:55 +1000

    nix-build now works

--- default.nix
+++ default.nix
@@ -4,24 +4,33 @@
 let
   inherit (nixpkgs) pkgs;
 
-  haskellPackages = if compiler == "default"
-                    then pkgs.haskellPackages
-                    else pkgs.haskell.packages.${compiler};
+  haskellPackages =
+    if compiler == "default"
+    then pkgs.haskellPackages
+    else pkgs.haskell.packages.${compiler};
 
   hpython = pkgs.fetchFromGitHub {
     owner = "qfpl";
     repo = "hpython";
-    rev = "49bd11644bbda2a7d3732180212ecad08e2a7760"; # develop
-    sha256 = "0b7kzw42nkb50n3nlczll60imk7nhg746jh0ch4nn3b26xbwmwdp";
+    rev = "f1371dad6951343f804852f88c4cb8c78baae565"; # develop
+    sha256 = "1bzam5rccmgg74zwkd1ccmkx2cs4nvmh09qxw5xggf5dxz4a8wgl";
   };
 
-  modifiedHaskellPackages = haskellPackages.override {
-    overrides = self: super: {
-      digit          = self.callHackage "digit" "0.7" {};
-      hpython        = import hpython {};
-      validation = pkgs.haskell.lib.dontCheck (self.callHackage "validation" "1" {});
-    };
+  validation = pkgs.fetchFromGitHub {
+    owner = "qfpl";
+    repo = "validation";
+    rev = "b7c5c0cf7d0bdbd8c0c47ac03d3a83bc75fc5f57";
+    sha256 = "1hkd7ma38rdq9rw0bbivi3l33i01idy5qka2x9fmzvfkjzr17q1q";
   };
+
+  modifiedHaskellPackages =
+    haskellPackages.override {
+      overrides = self: super: {
+        hpython = pkgs.haskell.lib.dontCheck (self.callCabal2nix "hpython" hpython {});
+	hedgehog = self.callHackage "hedgehog" "0.6" {};
+        validation = self.callCabal2nix "validation" validation {};
+      };
+    };
 
   drv = modifiedHaskellPackages.callPackage ./reindent.nix {};
 
